1a9ad61ab81581b2e3c6a299b294f5c3867647a6
abiinavvv <abhinavaneesh2@gmail.com>
2026-07-10 13:52:11 +0530

    little change

--- lib/psychologists.ts
+++ lib/psychologists.ts
@@ -37,7 +37,7 @@ export const psychologists: Psychologist[] = [
   {
     name: "Kripa K A",
     slug: "kripa-k-a",
-    designation: "Clinical Psychologist",
+    designation: "Clinical Psychologist Trainee",
     credentials: "M.Phil Psychology",
     rciNumber: "",
     photo: "/images/psychologists/kripa-k-a.png",
